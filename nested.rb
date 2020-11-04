a5ad3681470b0cf799f803f4ed7918e0e29c505c
Daniel Yankiver <dyankiver@gmail.com>
2020-11-04 21:59:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    changing_alan = programmer_hash[:alan_kay][:known_for] = "GUI"
+    changing_alan = programmer_hash[:alan_kay][:known_for]
+    changing_alan = "GUI"
     programmer_hash
 end
